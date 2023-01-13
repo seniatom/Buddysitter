1edcba703ae073c39878f65248c261e48fa661e6
seniatom <39411453+seniatom@users.noreply.github.com>
2023-01-13 00:25:05 +0000

    start and stop functions

--- CMotor.cpp
+++ CMotor.cpp
@@ -20,6 +20,19 @@ void CMotor::StartMotor()
     if(Motor_Status == false)
     {
         //start motor dd
+        if (!bcm2835_init())
+	        return; //return 1
+        
+        bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_ALT5);
+        
+        bcm2835_pwm_set_clock(BCM2835_PWM_CLOCK_DIVIDER_32);
+        bcm2835_pwm_set_mode(PWM_CHANNEL, 1, 1);
+        bcm2835_pwm_set_range(PWM_CHANNEL, RANGE);
+        
+        int data = 512;
+        bcm2835_pwm_set_data(PWM_CHANNEL, data);
+	    bcm2835_delay(1);
+        
     }
     Motor_Status = true;
 }
@@ -27,6 +40,11 @@ void CMotor::StartMotor()
 void CMotor::StopMotor()
 {
     //stop motor dd
+    int data = 0;
+    bcm2835_pwm_set_data(PWM_CHANNEL, data);
+	bcm2835_delay(1);
+    bcm2835_close();
+    
     Motor_Status = false;
 }
 
