95c675b93b1d16c426e470e5e79cca1051b42a4c
nenere1 <97645927+nenere1@users.noreply.github.com>
2023-02-11 20:00:39 +0000

    Update CHX711.h

--- CHX711.h
+++ CHX711.h
@@ -3,17 +3,24 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <iostream>
 #include <unistd.h> 
 #include <sys/wait.h>
+#include <fcntl.h>
 
 class CHX711
 {
 public:
+    CHX711();
     ~CHX711();
-    void InitHX771();
-    void RemHX771();
+    void TareWeightSensor();
+    void CalibrateWeightSensor();
     int ReadWeightSample();
+    
+private:
+    int scale0;
+    int zero;
+    int calibration_value;
+    int WeightSensor_Status; 
 };
 
 #endif
